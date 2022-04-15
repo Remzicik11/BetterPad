b61bf5c01d6129fa006c561dee71a1576cd60ab9
Remzi Studios <rorobaba178@gmail.com>
2022-04-15 17:59:01 +0200

    removed

--- NPCore/Attributes/Optional.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NPCore.Internal
-{
-    [AttributeUsage(AttributeTargets.All)]
-    public class Optional : Attribute { }
-}
